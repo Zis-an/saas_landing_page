dab098a2f0783e0e05128ec3ab168397f1592284
Zis-an <zahid.officialuse@gmail.com>
2025-04-09 18:03:05 +0600

    1.21.08

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link as LinkScroll } from "react-scroll";
 import clsx from "clsx";
 
@@ -11,10 +11,23 @@ const NavLink = ({ title }) => (
 
 const Header = () => {
 
+    const [hasScrolled, setHasScrolled] = useState(false);
     const [isOpen, setIsOpen] =  useState(false);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            setHasScrolled(window.scrollY > 32)
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="fixed top-0 left-0 z-50 w-full py-10">
+        <header className={clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
             <div className="contianer flex h-14 items-center max-lg:px-5">
                 <a className="lg:hidden flex-1 cursor-pointer z-2">
                     <img src="/images/xora.svg" width={115} height={55} alt="logo" />
@@ -29,7 +42,13 @@ const Header = () => {
                                     <NavLink title="pricing"></NavLink>
                                 </li>
                                 <li className="nav-logo">
-                                    <LinkScroll>
+                                    <LinkScroll 
+                                        to="hero" 
+                                        offset={-250} 
+                                        spy 
+                                        smooth 
+                                        className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}
+                                    >
                                         <img
                                             src="/images/xora.svg"
                                             width={160}
@@ -45,6 +64,20 @@ const Header = () => {
                                 </li>
                             </ul>
                         </nav>
+
+                        <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+                            <img src="/images/bg-outlines.svg" 
+                            width={960} 
+                            height={380}  
+                            alt="outline" 
+                            className="relative z-2" />
+                            
+                            <img src="/images/bg-outlines-fill.png" 
+                            width={960} 
+                            height={380}  
+                            alt="outline" 
+                            className="absolute inset-0 mix-blend-soft-light opacity-5" />
+                        </div>
                     </div>
                 </div>
 

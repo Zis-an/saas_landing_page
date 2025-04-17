2302317f450baab5ded83a7f8ba191cb0fd7a150
Zis-an <zahid.officialuse@gmail.com>
2025-04-17 17:05:24 +0600

    completed

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
     // Helps to quickly scroll to anywhere on the page
     const NavLink = ({ title }) => (
         <LinkScroll
-        onClick={() => (setIsOpen(false))}
+            onClick={() => (setIsOpen(false))}
             to={title}
             offset={-100}
             spy
@@ -37,7 +37,7 @@ const Header = () => {
 
     return (
         <header className={clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
-            <div className="contianer flex h-14 items-center max-lg:px-5">
+            <div className="container flex h-14 items-center max-lg:px-5">
                 <a className="lg:hidden flex-1 cursor-pointer z-2">
                     <img src="/images/xora.svg" width={115} height={55} alt="logo" />
                 </a>
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,4 +1,5 @@
-import { testimonials } from "../constants";
+import { testimonials } from "../constants/index.jsx";
+import TestimonialItem from "../components/TestimonialItem.jsx";
 
 
 const Testimonials = () => {
@@ -15,7 +16,11 @@ const Testimonials = () => {
 
                 <div className="testimonials_inner-after testimonials_inner-before relative -my-12 -mr-3 flex items-start max-lg:static max-md:block">
                     <div className="testimonials_group-after flex-50">
-                        {testimonials.slice(0, halfLength. map(() => {})) }
+                        {testimonials.slice(0, halfLength).map((testimonial) => (<TestimonialItem key={testimonial.id} item={testimonial} containerClassName="last:after:hidden last:after:max-md-block" />))}
+                    </div>
+
+                    <div className="flex-50 ">
+                        {testimonials.slice(halfLength).map((testimonial) => (<TestimonialItem key={testimonial.id} item={testimonial} containerClassName="last:after:hidden after:right-auto after:left-0 after:max-md:-left-4 md:px-12" />))}
                     </div>
                 </div>
             </div>
